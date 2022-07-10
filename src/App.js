b1fa99931fc9b3168eedc2cd293f089a35ea613e
Gosia Krawczuk <gosiek.krawczuk@gmail.com>
2022-07-10 20:51:57 +0200

    two ways to fetch data but anyone is good

--- src/App.js
+++ src/App.js
@@ -7,28 +7,44 @@ import './App.css'
 
 function App() {
   const [people, setPeople] = useState([]);
-
-  
-
     const fetchAll = async()=> {
-      const urls = [
-        "https://swapi.dev/api/people/1/",
-        "https://swapi.dev/api/people/2/",
-        "https://swapi.dev/api/people/3/",
-        "https://swapi.dev/api/people/4/",
-        "https://swapi.dev/api/people/5/",
-        "https://swapi.dev/api/people/6/",
-        "https://swapi.dev/api/people/7/",
-        "https://swapi.dev/api/people/8/",
-        "https://swapi.dev/api/people/9/"
-      ]
+      // SPOSOB NR 1:
+      // const urls = [ 
+      //   "https://swapi.dev/api/people/1/",
+      //   "https://swapi.dev/api/people/2/",
+      //   "https://swapi.dev/api/people/3/",
+      //   "https://swapi.dev/api/people/4/",
+      //   "https://swapi.dev/api/people/5/",
+      //   "https://swapi.dev/api/people/6/",
+      //   "https://swapi.dev/api/people/7/",
+      //   "https://swapi.dev/api/people/8/",
+      //   "https://swapi.dev/api/people/9/"
+      // ]
 
       try {
-        const response = await Promise.all(urls.map((url) => fetch(url).then((response) => response.json()
-        )));
+        // CD SPOSOBU NR 1
+        // const response = await Promise.all(urls.map((url) => fetch(url).then((response) => response.json()
+        // )));
+        // SPOSÓB NR 2:
+        const url = "https://swapi.dev/api/people/";
+
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json()
+        console.log(data);
+        // console.log(data.results);
     
-        console.log(response);
-        setPeople(response);
+        // console.log(response);
+        let slicedArray = [];
+        let chunkSize = 9;
+        for(let i = 0; i<response.length; i++){
+          const chunk = response.slice(i, i+chunkSize);
+          slicedArray.push(chunk);
+          // console.log(slicedArray)
+        }
+        // setPeople(response);
       } catch(error) {
           console.error("error",error);
         }
